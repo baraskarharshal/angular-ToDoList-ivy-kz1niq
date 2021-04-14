32fae4434e86d95efe4d7256f2f26b3f75fc80c4
baraskarharshal <baraskarharshal@gmail.com>
2021-04-14 20:01:34 +0530

    3 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TodolistPageComponent } from './todolist-page/todolist-page.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TodolistPageComponent, TodoListComponent, TodoListItemComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/todolist-page/todolist-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todolist-page',
+  templateUrl: './todolist-page.component.html',
+  styleUrls: ['./todolist-page.component.css']
+})
+export class TodolistPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
